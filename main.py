029b032b8155609a4c6ee7656ab6772044874b21
SK-Fast <60780448+SK-Fast@users.noreply.github.com>
2021-06-27 13:34:36 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#Variables
+# Variables
 api = "rUZxDJGJOUHcd1Tby5sRuQADsRwXLAZ7"
 delay = 5
-
-
+watertime = 1
 delay = delay * 1000
+watertime = watertime * 1000
 basic.show_leds("""
     . . . . .
     . . . . .
@@ -23,13 +23,39 @@ def on_forever():
             . # . . .
             """)
         basic.pause(delay)
+        WifiModule.write_blynk_pin_value(api, "V0", "Idle")
+        basic.pause(delay)
         readforce = WifiModule.read_blynk_pin_value(api, "V1")
         if readforce == "1":
-            pins.digital_read_pin(DigitalPin.P1)
+            WifiModule.write_blynk_pin_value(api, "V0", "Watering...")
+            basic.show_leds("""
+                . . # . .
+                . # # # .
+                # # # . #
+                # # . # #
+                . # # # .
+                """)
+            basic.pause(delay)
+            pins.digital_write_pin(DigitalPin.P1, 1)
+            basic.pause(watertime)
+            pins.digital_write_pin(DigitalPin.P1, 0)
+            basic.pause(delay - watertime)
+            WifiModule.write_blynk_pin_value(api, "V1", "0")
+            basic.pause(delay)
+            WifiModule.write_blynk_pin_value(api, "V0", "Idle")
+            basic.show_leds("""
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            """)
         basic.pause(delay)
         readmositure = pins.analog_read_pin(AnalogPin.P2)
-        WifiModule.write_blynk_pin_value(api, "V0", str(readmositure))
+        WifiModule.write_blynk_pin_value(api, "V0", "" + str(readmositure))
         basic.pause(delay)
+        brightness = input.light_level()
+        WifiModule.write_blynk_pin_value(api, "V0", "" + str(brightness))
     else:
         basic.show_leds("""
             . . . . .
@@ -39,3 +65,4 @@ def on_forever():
             . . . . .
             """)
 basic.forever(on_forever)
+
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-// Variables
+//  Variables
 let api = "rUZxDJGJOUHcd1Tby5sRuQADsRwXLAZ7"
 let delay = 5
+let watertime = 1
 delay = delay * 1000
+watertime = watertime * 1000
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -13,6 +15,7 @@ WifiModule.setupWifi(SerialPin.P13, SerialPin.P12, "mSPACE", "18871008")
 basic.forever(function on_forever() {
     let readforce: string;
     let readmositure: number;
+    let brightness: number;
     if (WifiModule.isConnected()) {
         basic.showLeds(`
             . . . . .
@@ -22,15 +25,41 @@ basic.forever(function on_forever() {
             . # . . .
             `)
         basic.pause(delay)
+        WifiModule.writeBlynkPinValue(api, "V0", "Idle")
+        basic.pause(delay)
         readforce = WifiModule.readBlynkPinValue(api, "V1")
         if (readforce == "1") {
-            pins.digitalReadPin(DigitalPin.P1)
+            WifiModule.writeBlynkPinValue(api, "V0", "Watering...")
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # # # . #
+                # # . # #
+                . # # # .
+                `)
+            basic.pause(delay)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            basic.pause(watertime)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            basic.pause(delay - watertime)
+            WifiModule.writeBlynkPinValue(api, "V1", "0")
+            basic.pause(delay)
+            WifiModule.writeBlynkPinValue(api, "V0", "Idle")
+            basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
         }
         
         basic.pause(delay)
         readmositure = pins.analogReadPin(AnalogPin.P2)
-        WifiModule.writeBlynkPinValue(api, "V0", "" + readmositure)
+        WifiModule.writeBlynkPinValue(api, "V0", "" + ("" + readmositure))
         basic.pause(delay)
+        brightness = input.lightLevel()
+        WifiModule.writeBlynkPinValue(api, "V0", "" + ("" + brightness))
     } else {
         basic.showLeds(`
             . . . . .
