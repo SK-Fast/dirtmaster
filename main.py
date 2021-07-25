23cb46427b5c7edc73acfd686a61b04566fb9949
SK-Fast <60780448+SK-Fast@users.noreply.github.com>
2021-07-25 11:32:57 +0700

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Variables
 api = "rUZxDJGJOUHcd1Tby5sRuQADsRwXLAZ7"
 delay = 5
-watertime = 1
+watertime = 5
 
 
 switcha = False
 switchb = False
+isforcelight = False
 delay = delay * 1000
 watertime = watertime * 1000
 basic.show_leds("""
@@ -17,9 +18,36 @@ basic.show_leds("""
     """)
 pins.digital_write_pin(DigitalPin.P1, 0)
 pins.digital_write_pin(DigitalPin.P8, 0)
-WifiModule.setup_wifi(SerialPin.P13, SerialPin.P12, "mSPACE", "18871008")
+WifiModule.setup_wifi(SerialPin.P13, SerialPin.P12, "THANJAITECHNOLOGY_2.4G", "0899305335")
+
+def action_water():
+    WifiModule.write_blynk_pin_value(api, "V3", "Watering...")
+    basic.show_leds("""
+                    . . # . .
+                    . # # # .
+                    # # # . #
+                    # # . # #
+                    . # # # .
+                    """)
+    basic.pause(delay)
+    pins.digital_write_pin(DigitalPin.P1, 1)
+    basic.pause(watertime)
+    pins.digital_write_pin(DigitalPin.P1, 0)
+    basic.pause(delay - watertime)
+    WifiModule.write_blynk_pin_value(api, "V1", "0")
+    basic.pause(delay)
+    WifiModule.write_blynk_pin_value(api, "V3", "Idle")
+    basic.show_leds("""
+                . . . . .
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                """)
+
 
 def on_forever():
+    global isforcelight
     if WifiModule.is_connected():
         basic.show_leds("""
             . . . . .
@@ -29,45 +57,31 @@ def on_forever():
             . # . . .
             """)
         basic.pause(delay)
-        WifiModule.write_blynk_pin_value(api, "V0", "Idle")
+        WifiModule.write_blynk_pin_value(api, "V3", "Idle")
         basic.pause(delay)
-        readforce = WifiModule.read_blynk_pin_value(api, "V1")
+        readforce = WifiModule.read_blynk_pin_value(api, "V1") # Water
         if readforce == "1":
-            WifiModule.write_blynk_pin_value(api, "V0", "Watering...")
-            basic.show_leds("""
-                . . # . .
-                . # # # .
-                # # # . #
-                # # . # #
-                . # # # .
-                """)
-            basic.pause(delay)
-            pins.digital_write_pin(DigitalPin.P1, 1)
-            basic.pause(watertime)
-            pins.digital_write_pin(DigitalPin.P1, 0)
-            basic.pause(delay - watertime)
-            WifiModule.write_blynk_pin_value(api, "V1", "0")
-            basic.pause(delay)
-            WifiModule.write_blynk_pin_value(api, "V0", "Idle")
-            basic.show_leds("""
-            . . . . .
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            """)
+            action_water()
         basic.pause(delay)
-        readforce = WifiModule.read_blynk_pin_value(api, "V5")
+        readforce = WifiModule.read_blynk_pin_value(api, "V5") #Light
         if readforce == "1":
+            isforcelight = True
             pins.digital_write_pin(DigitalPin.P8, 1)
         else:
             pins.digital_write_pin(DigitalPin.P8, 0)
         basic.pause(delay)
         readmositure = pins.analog_read_pin(AnalogPin.P2)
         WifiModule.write_blynk_pin_value(api, "V0", "" + str(readmositure))
+        if readmositure > 1000:
+            action_water()
         basic.pause(delay)
         brightness = input.light_level()
         WifiModule.write_blynk_pin_value(api, "V0", "" + str(brightness))
+        if isforcelight == False:
+            if brightness < 20:
+                pins.digital_write_pin(DigitalPin.P8, 1)
+            else:
+                pins.digital_write_pin(DigitalPin.P8, 0)
     else:
         basic.show_leds("""
             . . . . .
--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 //  Variables
 let api = "rUZxDJGJOUHcd1Tby5sRuQADsRwXLAZ7"
 let delay = 5
-let watertime = 1
+let watertime = 5
 let switcha = false
 let switchb = false
+let isforcelight = false
 delay = delay * 1000
 watertime = watertime * 1000
 basic.showLeds(`
@@ -15,11 +16,38 @@ basic.showLeds(`
     `)
 pins.digitalWritePin(DigitalPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P8, 0)
-WifiModule.setupWifi(SerialPin.P13, SerialPin.P12, "mSPACE", "18871008")
+WifiModule.setupWifi(SerialPin.P13, SerialPin.P12, "THANJAITECHNOLOGY_2.4G", "0899305335")
+function action_water() {
+    WifiModule.writeBlynkPinValue(api, "V3", "Watering...")
+    basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # # # . #
+                    # # . # #
+                    . # # # .
+                    `)
+    basic.pause(delay)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(watertime)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(delay - watertime)
+    WifiModule.writeBlynkPinValue(api, "V1", "0")
+    basic.pause(delay)
+    WifiModule.writeBlynkPinValue(api, "V3", "Idle")
+    basic.showLeds(`
+                . . . . .
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                `)
+}
+
 basic.forever(function on_forever() {
     let readforce: string;
     let readmositure: number;
     let brightness: number;
+    
     if (WifiModule.isConnected()) {
         basic.showLeds(`
             . . . . .
@@ -29,38 +57,19 @@ basic.forever(function on_forever() {
             . # . . .
             `)
         basic.pause(delay)
-        WifiModule.writeBlynkPinValue(api, "V0", "Idle")
+        WifiModule.writeBlynkPinValue(api, "V3", "Idle")
         basic.pause(delay)
         readforce = WifiModule.readBlynkPinValue(api, "V1")
+        //  Water
         if (readforce == "1") {
-            WifiModule.writeBlynkPinValue(api, "V0", "Watering...")
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # # # . #
-                # # . # #
-                . # # # .
-                `)
-            basic.pause(delay)
-            pins.digitalWritePin(DigitalPin.P1, 1)
-            basic.pause(watertime)
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            basic.pause(delay - watertime)
-            WifiModule.writeBlynkPinValue(api, "V1", "0")
-            basic.pause(delay)
-            WifiModule.writeBlynkPinValue(api, "V0", "Idle")
-            basic.showLeds(`
-            . . . . .
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            `)
+            action_water()
         }
         
         basic.pause(delay)
         readforce = WifiModule.readBlynkPinValue(api, "V5")
+        // Light
         if (readforce == "1") {
+            isforcelight = true
             pins.digitalWritePin(DigitalPin.P8, 1)
         } else {
             pins.digitalWritePin(DigitalPin.P8, 0)
@@ -69,9 +78,22 @@ basic.forever(function on_forever() {
         basic.pause(delay)
         readmositure = pins.analogReadPin(AnalogPin.P2)
         WifiModule.writeBlynkPinValue(api, "V0", "" + ("" + readmositure))
+        if (readmositure > 1000) {
+            action_water()
+        }
+        
         basic.pause(delay)
         brightness = input.lightLevel()
         WifiModule.writeBlynkPinValue(api, "V0", "" + ("" + brightness))
+        if (isforcelight == false) {
+            if (brightness < 20) {
+                pins.digitalWritePin(DigitalPin.P8, 1)
+            } else {
+                pins.digitalWritePin(DigitalPin.P8, 0)
+            }
+            
+        }
+        
     } else {
         basic.showLeds(`
             . . . . .
